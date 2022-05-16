abe78a2f3dc592a67ab37c19b8aa314a5bcc996b
PEDRO AUGUSTO DA SILVA GUEDES <pedro.asguedes@senacsp.edu.br>
2022-05-16 16:56:50 -0300

    Final projeto

---
+++ agenda.sql
@@ -0,0 +1,17 @@
+/**
+Agenda de contatos
+@author Pedro Guedes
+*/
+
+-- Verificar bancos de dados
+show databases;
+
+-- Criar um novo banco de dados
+create database teste;
+create database agendaguedes;
+
+-- Excluir um bancode dados
+drop database teste;
+
+-- Selecionar o banco de dados
+use agendaguedes;
