f514d1694b544dbbee197dd8b08a25f567c417ce
Pedro Guedes <87093449+pedroasguedes@users.noreply.github.com>
2022-05-18 16:43:49 -0300

    Add files via upload

---
+++ loja/lojinha.sql
@@ -0,0 +1,12 @@
+/**
+Lojinha
+@author Pedro Guedes
+*/
+
+
+create database lojaguedes;
+use  lojaguedes;
+create table produtos (id int primary key auto_increment, prod varchar(255) not null, valor decimal not null,estoq int );
+show tables;
+describe loja;
+
